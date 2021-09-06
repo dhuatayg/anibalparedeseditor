7a92fc94eff8a81f1e0bf42587eced192e0752b1
Diego Huatay <diego.huatay.contractor@bbva.com>
2021-09-06 00:40:44 -0500

    Login

---
+++ application/views/core/login.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Anibal Paredes Editor S.A.C.</title>
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?php echo base_url();?>plugins/fontawesome-free/css/all.min.css">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="<?php echo base_url();?>plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?php echo base_url();?>plugins/dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition login-page">
+        <div class="login-box">
+        <!-- /.login-logo -->
+            <div class="card card-outline card-primary">
+                <div class="card-header text-center">
+                    <a href="../../index2.html" class="h4"><b>Anibal Paredes Editor S.A.C.</a>
+                </div>
+                <div class="card-body">
+                    <p class="login-box-msg">Ingrese sus credenciales</p>
+                    <form action="../../index3.html" method="post">
+                        <div class="input-group mb-3">
+                            <input type="email" class="form-control" placeholder="Correo">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-envelope"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="password" class="form-control" placeholder="Clave">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-lock"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <!-- /.col -->
+                            <div class="col-12">
+                                <button type="submit" class="btn btn-primary btn-block">Iniciar sesión</button>
+                            </div>
+                        <!-- /.col -->
+                        </div>
+                    </form>
+                </div>
+                <!-- /.card-body -->
+            </div>
+        <!-- /.card -->
+        </div>
+    <!-- /.login-box -->
+
+    <!-- jQuery -->
+    <script src="<?php echo base_url();?>plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="<?php echo base_url();?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="<?php echo base_url();?>plugins/dist/js/adminlte.min.js"></script>
+</body>
+</html>
+
